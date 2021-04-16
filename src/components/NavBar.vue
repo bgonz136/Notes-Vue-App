88db1f3f18c6f98c573fbb8ab2e3e9359f38121a
Brian <bgonz136@gmail.com>
2021-04-15 20:31:10 -0500

    Added API connection

---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-red-500 p-4">
+    <div
+      class="flex place-items-center space-x-2 text-coolGray-100 text-3xl container mx-auto"
+    >
+      <ph:notepad-duotone />
+      <p class="font-bold">
+        BG<span class="font-thin tracking-wider">Notes</span>
+      </p>
+    </div>
+  </div>
+</template>
